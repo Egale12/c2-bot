4b36e642d202fe89c68168a4f13e810c7890af6c
Egale12 <Mohammedsoaldarei@hotmail.com>
2026-06-14 17:54:10 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,14 @@ intents.members = True
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_member_join(member):
+
+@bot.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.text_channels, name="welcome")
 
