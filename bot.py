39f6a67c332720b118012aea728585806b540054
Egale12 <Mohammedsoaldarei@hotmail.com>
2026-06-14 17:04:33 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,7 +146,7 @@ async def on_voice_state_update(member, before, after):
 
 
 
-@bot.command(name="c2help")
+@bot.command(name="c2")
 async def c2help(ctx):
     embed = discord.Embed(
         title="📖 C2 Help Menu",
